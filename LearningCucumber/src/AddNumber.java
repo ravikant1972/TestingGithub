f81036db836384dba5af30e8a3dcb7a4a6364caf
ravikant dongre <dongre.ravikant@gmail.com>
2018-07-24 17:11:25 +0530

    First modification

---
+++ LearningCucumber/src/AddNumber.java
@@ -0,0 +1,20 @@
+
+public class AddNumber {
+public void addnum(){
+	
+	int a=2,b=4;
+	
+	int c=a+b;
+	
+	System.out.println(c);
+}
+	
+	public static void main(String[] args) {
+		
+		AddNumber abc = new AddNumber();
+		
+		abc.addnum();
+		
+	}
+
+}
